d4ce1f43837b5b0f8f7d35c68ea86d02dd0100cd
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2025-06-07 18:34:57 +0900

    Add feature: Downloading dataset

--- src/LuxBPNet.jl
+++ src/LuxBPNet.jl
@@ -1,6 +1,7 @@
 module LuxBPNet
 
 using Downloads
+using BPNET_jll: BPNET_jll
 using Tar: Tar
 using CodecBzip2: CodecBzip2
 using TranscodingStreams: TranscodingStreams
@@ -8,31 +9,12 @@ using Scratch: @get_scratch!
 
 const DATASET_DIR = Ref{String}()
 
-function download_dataset()
-	url = "http://ann.atomistic.net/files/aenet-example-02-TiO2-Chebyshev.tar.bz2"
-	bz2name = "aenet-example-02-TiO2-Chebyshev.tar.bz2"
-	tarname = first(splitext(bz2name))
-	bz2path = joinpath(DATASET_DIR[], bz2name)
-	tarpath = joinpath(DATASET_DIR[], tarname)
-	extractpath = joinpath(DATASET_DIR[], "extracted_files")
-	if !isfile(bz2path)
-		Downloads.download(url, bz2path)
-		open(bz2path) do inp
-			open(tarpath, "w") do output_file
-	            stream = TranscodingStreams.TranscodingStream(CodecBzip2.Bzip2Decompressor(), inp)
-	            write(output_file, stream)
-	        end
-	    end
-	    # Extract to the new directory
-	    Tar.extract(tarpath, extractpath)
-		@info "Done!"
-	else
-		@info "Dataset exists in $(bz2path)"
-	end
-end
+include("data_downloader.jl")
+include("fingerprints.jl")
+include("data_generator.jl")
 
 function __init__()
-	global DATASET_DIR
+    global DATASET_DIR
     DATASET_DIR[] = @get_scratch!("dataset")
 end
 
